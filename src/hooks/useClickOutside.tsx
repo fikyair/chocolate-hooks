9b45a3cd521a4369eb428e54c8b902d2f54aef52
fikyair <shiming_xsm@163.com>
2020-12-14 15:58:04 +0800

    useClickOutside-hook

---
+++ src/hooks/useClickOutside.tsx
@@ -0,0 +1,21 @@
+/**
+ * 使用场景：通过获得组件 ref，使得组件从展示到消失
+ */
+import { RefObject, useEffect } from "react";
+
+function useClickOutside(ref: RefObject<HTMLElement>, handler: Function) {
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (!ref.current || ref.current.contains(event.target as HTMLElement)) {
+        return
+      }
+      handler(event)
+    }
+    document.addEventListener('click', listener)
+    return () => {
+      document.removeEventListener('click', listener)
+    }
+  }, [ref, handler])
+}
+
+export default useClickOutside
